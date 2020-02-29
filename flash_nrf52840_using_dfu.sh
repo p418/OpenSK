0da473d93a5c8da33136f607a0f7408c694ca232
Dennis Geurts <dennisg@dennisg.nl>
2020-02-29 15:52:17 +0100

    untested, but merged the fork to hopefully reflect the changes in the main project

--- flash_nrf52840_using_dfu.sh
+++ flash_nrf52840_using_dfu.sh
@@ -13,7 +13,7 @@ make -C third_party/tock/boards/nordic/nrf52840_dongle program
 cp third_party/tock/boards/nordic/nrf52840_dongle/target/thumbv7em-none-eabi/release/nrf52840_dongle.hex $OUTPUT/nrf52840_dongle.hex
 
 #then generate the openSK app
-board=nrf52840_dongle ./deploy.sh app
+./deploy.py os --board=nrf52840_dongle
 
 # generate hex files, with relocation
 /usr/local/bin/bin2hex.py --offset=0x30000 target/tab/padding.bin $OUTPUT/padding.hex
